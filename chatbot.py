23aa8e32d139c66f97055d21cc4972b5fdf6281d
zoyalniha-prog <zoyalniha@gmail.com>
2026-02-28 14:48:16 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
